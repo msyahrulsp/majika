d34ab23d2422c7def6f7f531bd9f3c52da7d93a7
msyahrulsp <13520161@std.stei.itb.ac.id>
2023-02-24 02:19:49 +0700

    feat: add splash screen

---
+++ app/src/main/java/com/example/majika/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.majika
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+@Suppress("DEPRECATION")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
